318eb9dd098225f5caeaee3dc7a81467882482dc
BalenovicMarko <marko.balenovic@student.uniri.hr>
2024-11-25 19:22:31 +0100

    Updated

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Destination, Booking
+from .models import Destination, Booking, Customer, Service
 
 admin.site.register(Destination)
 admin.site.register(Booking)
+admin.site.register(Customer)
+admin.site.register(Service)
